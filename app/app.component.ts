7eedffaef0ce7b45156f60de3109e7f2545b29d8
Ming Wen and Scott Holland <mwen82@gmail.com>
2016-06-01 11:43:43 -0700

    Add select functionality with styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,24 +1,40 @@
-import { Component } from 'angular2/core';
+import { Component, EventEmitter } from 'angular2/core';
 
-export class Album {
-  public cart: boolean = false;
-  constructor(public artist: string, public title: string, public genre: string, public price: number, public id: number) {
-
-  }
+@Component({
+    selector: 'album-display',
+    inputs: ['album'],
+  template: `
+    <h3>{{ album.title }}</h3>
+  `
+})
+export class AlbumComponent {
+  public album: Album;
 }
+
 @Component({
   selector: 'album-list',
   inputs: ['albumList'],
+  outputs: ['onAlbumSelect'],
+  directives: [AlbumComponent],
   template: `
-  <h3 *ngFor="#currentAlbum of albumList" (click)="albumClicked(currentAlbum)">
-    {{ currentAlbum.title }}
-  </h3>
+  <album-display *ngFor="#currentAlbum of albumList"
+    (click)="albumClicked(currentAlbum)"
+    [class.selected]="currentAlbum === selectedAlbum"
+    [album]="currentAlbum">
+  </album-display>
   `
 })
 export class AlbumListComponent {
   public albumList: Album[];
+  public onAlbumSelect: EventEmitter<Album>;
+  public selectedAlbum: Album;
+  constructor() {
+    this.onAlbumSelect = new EventEmitter();
+  }
   albumClicked(clickedAlbum: Album): void {
-    console.log(clickedAlbum);
+    console.log('child', clickedAlbum);
+    this.selectedAlbum = clickedAlbum;
+    this.onAlbumSelect.emit(clickedAlbum);
   }
 }
 
@@ -26,10 +42,13 @@ export class AlbumListComponent {
   selector: 'my-app',
   directives: [AlbumListComponent],
   template: `
-  <div class="container">
-    <h1>Music Store!</h1>
-    <album-list [albumList]="albums"></album-list>
-  </div>
+    <div class="container">
+      <h1>Music Store!</h1>
+      <album-list
+        [albumList]="albums"
+        (onAlbumSelect)="albumWasSelected($event)">
+      </album-list>
+    </div>
   `
 })
 export class AppComponent {
@@ -41,6 +60,13 @@ export class AppComponent {
     ];
   }
   albumWasSelected(clickedAlbum: Album): void {
-    console.log(clickedAlbum);
+    console.log('parent', clickedAlbum);
+  }
+}
+
+export class Album {
+  public cart: boolean = false;
+  constructor(public artist: string, public title: string, public genre: string, public price: number, public id: number) {
+
   }
 }
